95fa25413d81defa728a92eb05449581dc350f0c
moizes86 <moshe.mn86@gmail.com>
2021-07-06 22:06:18 +0300

    merge with server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { signup, login, updateDetails } = require("../db");
 const router = express.Router();
-const {validateInput} = require( '../../validations');
+const {
+  validateEmail,
+  validateUsername,
+  validatePassword,
+  validateConfirmPassword,
+} = require("../../validations");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -12,11 +17,10 @@ router.get("/", function (req, res, next) {
 router.post("/signup", async (req, res) => {
   try {
     const { email, username, password, confirmPassword } = req.body;
-     validateInput('email', email);
-     validateInput('username', username);
-     validateInput('password', password);
-     validateInput("confirmPassword", confirmPassword, password);
-
+    validateEmail(email);
+    validateUsername(username);
+    validatePassword(password);
+    validateConfirmPassword(confirmPassword, password);
 
     const result = await signup(email, username, password);
     res.status(200).json(result);
@@ -25,17 +29,16 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-/* GET user by id or email and password. */
 /* Login */
 router.post("/login", async (req, res) => {
   try {
-    const { id, email, password } = req.body;
+    const { email, password } = req.body;
 
-    validateInput('email', email);
-    validateInput('password',password);
+    validateEmail(email);
+    validatePassword(password);
 
     const result = await login(email, password);
-    if (!result.length) res.status(500).json({err: 'Username or password incorrect'})
+    if (!result.length) res.status(500).json({ err: "Username or password incorrect" });
     res.status(200).json(result);
   } catch (e) {
     res.status(500).json({ err: e.message });
@@ -45,11 +48,11 @@ router.post("/login", async (req, res) => {
 /* Update user's details */
 router.put("/update-details", async (req, res) => {
   try {
-    const { id, username, password,confirmPassword } = req.body;
+    const { id, username, password, confirmPassword } = req.body;
 
-    validateInput("username", username);
-    validateInput("password", password);
-    validateInput("confirmPassword", confirmPassword, password);
+    validateUsername(username);
+    validatePassword(password);
+    validateConfirmPassword(confirmPassword, password);
 
     const result = await updateDetails(id, username, password);
     res.status(200).json(result);

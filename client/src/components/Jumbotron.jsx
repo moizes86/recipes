b15854ce1b2ad313327486d4950bf446dac0820b
moizes86 <moshe.mn86@gmail.com>
2021-07-13 16:52:11 +0300

    Footer always at bottom, bg-img jumbo

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -5,10 +5,11 @@ import logo from "../assets/chef.png";
 const Jumbotron = () => {
   return (
     <div className="my-jumbotron">
+      <div className="background-img"></div>
       <div className="container">
         <img src={logo} alt="" />
         <h1>Recipes Farm</h1>
-        <h4>Your home for recipes</h4>
+        <h3>Your home for recipes</h3>
         <p>
           Recipe Farm is the place to get any recipe that comes to mind. You can also create your own recipe, upload and
           update it. Or don't. That's ok. We'll see what happens.
